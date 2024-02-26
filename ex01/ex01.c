adc9f4b1c848ae5449d063f3840aa6715475251d
Marcio Bueno <marciobueno@gmail.com>
2024-02-26 11:07:29 +0000

    exemplo básico + for mostrando problema de arredondamento quando se usa float

--- ex01/ex01.c
+++ ex01/ex01.c
@@ -1,6 +1,18 @@
 #include "stdio.h"
 
 int main() {
+  int num1;
+  printf("num1 = %d\n", num1);
+  num1 = 10;
+  printf("num1 = %d\n", num1);
+  printf("num1 = %f\n", num1);
   printf("Olá Turma!\n");
-  return 0;
+
+  float d = 0.0, inc = 0.1;
+  for (int i = 0; i < 100; ++i)
+  {
+    printf("%f\n", d);
+    d += inc;
+  }
+    return 0;
 }
