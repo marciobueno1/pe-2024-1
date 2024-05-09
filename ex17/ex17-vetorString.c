0adc0c159bfd7a01cb2078fa3655dd61d8d168d3
Marcio Bueno <marciobueno@gmail.com>
2024-05-09 08:44:45 -0300

    vetor de string - leitura e impressão

--- ex17/ex17-vetorString.c
+++ ex17/ex17-vetorString.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #define QTD_STRINGS 5
 #define QTD_CARACTERES 10
@@ -7,11 +10,17 @@ int main() {
   char codigos[QTD_STRINGS][QTD_CARACTERES];
   for (int i = 0; i < QTD_STRINGS; i++) {
     printf("Informe o código %i: ", i+1);
-    fgets_s(codigos[i], QTD_CARACTERES, stdin);
+    fgets(codigos[i], QTD_CARACTERES, stdin);
     int tam = strlen(codigos[i]);
     if (codigos[i][tam - 1] == '\n') {
       codigos[i][tam - 1] = '\0';
     }
   }
-  
+
+  printf("\n\nIMPRESSÃO:\n");
+  for (int i = 0; i < QTD_STRINGS; i++) {
+    printf("Código %i: %s\n", i+1, codigos[i]);
+  }
+
+  return 0;
 }
